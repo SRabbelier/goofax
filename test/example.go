67dfeda63a7bb4825d6bea7b33758c019cdb0010
Sverre Rabbelier <srabbelier@gmail.com>
2011-05-12 01:41:04 +0200

    Add example from go_spec
    Known breakages removed from example, most notably:
    * channel (operations)
    * automatic semicolon insertion

--- test/example.go
+++ test/example.go
@@ -1,18 +1,32 @@
-module example
+package foo
 
-// Example "Go" program (see syntax/Go.sdf for the grammar)
+import "fmt"
 
-entity User {
-  name     : String
-  password : String
-  homepage : URL
-}
+// Send the sequence 2, 3, 4, ... to channel 'ch'.
 
-entity BlogPosting {
-  poster : User
-  body   : String
-}
+// Copy the values from channel 'src' to channel 'dst',
+// removing those divisible by 'prime'.
+func filter() {
+	for i := range src {	// Loop over values received from 'src'.
+		if i%prime != 0 {
+			dst <- i  // Send 'i' to channel 'dst'.
+		}
+	}
+};
 
-entity URL {
-  location : String
+// The prime sieve: Daisy-chain filter processes together.
+func sieve() {
+	ch := make();  // Create a new channel.
+	go generate(ch);       // Start generate() as a subprocess.
+	for {
+		prime := <-ch;
+		Print(prime);
+		ch1 := make();
+		go filter(ch, ch1, prime);
+		ch = ch1;
+	}
+};
+
+func main() {
+	sieve()
 }
