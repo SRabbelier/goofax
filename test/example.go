04197cb2cb7f8ebfedfe56791669f2c401d60c7c
Sverre Rabbelier <srabbelier@gmail.com>
2011-05-18 16:32:21 -0700

    Crappy comment solution

--- test/example.go
+++ test/example.go
@@ -1,6 +1,6 @@
 package foo
 
-import "fmt";
+import "fmt"
 
 // Send the sequence 2, 3, 4, ... to channel 'ch'.
 
@@ -10,23 +10,23 @@ func filter() {
 	for i := range src {	// Loop over values received from 'src'.
 		if i%prime != 0 {
 			dst <- i;  // Send 'i' to channel 'dst'.
-		};
-	};
-};
+		}
+	}
+}
 
 // The prime sieve: Daisy-chain filter processes together.
 func sieve() {
-	ch := make();  // Create a new channel.
-	go generate(ch);       // Start generate() as a subprocess.
+	ch := make()  /* Create a new channel.
+	*/ go generate(ch)       // Start generate() as a subprocess.
 	for {
-		prime := <-ch;
-		Print(prime);
-		ch1 := make();
-		go filter(ch, ch1, prime);
-		ch = ch1;
-	};
-};
+		prime := <-ch
+		Print(prime)
+		ch1 := make()
+		go filter(ch, ch1, prime)
+		ch = ch1
+	}
+}
 
 func main() {
-	sieve();
-};
+	sieve()
+}
